738b1a98585af1c4ea7246f83b223c238c0d5960
Suriya <163859263+Suriya-7@users.noreply.github.com>
2025-07-23 11:29:40 +0530

    Update settings.py

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^m+ms#9p8lt+gk@g3g14n!4(b9hc*k*lcn^6f@-t0f&#h#oh&$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-weatherapp.onrender.com', 'localhost', '127.0.0.1v"]
 
 
 # Application definition
